5190a7e75e93e66985901db34b252ae6d01546af
ErickMwangi <rickthetri20@gmail.com>
2023-03-25 01:40:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOMM has loaded");
+    let parTag = document.getElementById("text")
+    parTag.textContent = "This is really cool!"
+})
+
+console.log("Load Dom before event listener")
